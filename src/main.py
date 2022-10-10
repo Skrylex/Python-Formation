fed817bab5f1340ba7612dcd2afd75935610e4a7
Skryle <deschamps.ghislain@gmail.com>
2022-10-10 12:14:50 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from person import Person
+
+## EXO 1
+persons = []
+persons.append(Person("deschamps", "ghislain", "france"))
+persons.append(Person("bejenne", "nicolas", "deutschland"))
+persons.append(Person("grossard", "romain", "italy"))
+persons.append(Person("bernard", "junior", "chinese"))
+
+for person in persons: print(person)
+
+## EXO 2
---
+++ src/person.py
@@ -0,0 +1,47 @@
+class Person:  
+  countries = ["FRANCE", "ITALY", "DEUTSCHLAND"]
+  id = 0
+  
+  def __init__(self, lastname, firstname, country = None) -> None:
+    self.lastname = lastname
+    self.firstname = firstname
+    self.country = country
+    
+    self._id = self.__class__.id
+    self.__class__.id += 1
+  
+  @property
+  def firstname(self) -> str: 
+    return self.__firstname.capitalize()
+  
+  @property
+  def lastname(self) -> str: 
+    return self.__lastname.capitalize()
+  
+  @property
+  def country(self) -> str:
+    if self.__country.upper() in self.countries:
+      return self.__country.capitalize()
+    else:
+      return None
+  
+  @firstname.setter
+  def firstname(self, firstname) -> None:
+    self.__firstname = firstname
+  
+  @lastname.setter
+  def lastname(self, lastname) -> None:
+    self.__lastname = lastname
+    
+  @country.setter
+  def country(self, country) -> None:
+    self.__country = country
+    
+  def fullname(self) -> str:
+    return f"{self.lastname} {self.firstname}"
+    
+  def __str__(self) -> str:
+    msg = f"#{self._id} : {self.fullname()}"
+    msg += f"et son pays est : {self.country}" if self.country else ""
+    return msg
+
